62b10666eb1a131f987858c220d21eeb446c109e
Gareth Murphy <garethcmurphy@gmail.com>
2018-09-18 10:47:06 +0200

    add mini sdk

--- index.ts
+++ index.ts
@@ -1,11 +1,21 @@
+import { PublishedData } from "./PublishedData";
+
 class MetadataCreator {
   metadata: Object;
+  Publish: PublishedData;
+
   constructor() {
-    this.metadata = { id: 20 };
+    this.Publish = new PublishedData();
+    this.Publish.creator = "J Smith";
+    this.Publish.publisher = "ESS";
+    this.Publish.affiliation = "ESS";
+    this.Publish.publicationYear = 2018;
+    this.Publish.pidArray = ["J Smith"];
+    this.metadata = this.Publish;
   }
 
   print() {
-    console.log(this.metadata);
+    console.log(this.Publish);
   }
 }
 
