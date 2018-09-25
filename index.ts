2d133a593bcb91d71eaa7c140e38945b6a16e48a
Gareth Murphy <garethcmurphy@gmail.com>
2018-09-25 04:28:23 +0200

    update types

--- index.ts
+++ index.ts
@@ -14,6 +14,16 @@ class MetadataCreator {
     this.metadata = this.Publish;
   }
 
+  mainloop() {
+    for (let i = 0; i < 5; i++) {
+      console.log(i);
+    }
+  }
+
+  get_file_info() {
+    // fs.readdirSync();
+  }
+
   print() {
     console.log(this.Publish);
   }
