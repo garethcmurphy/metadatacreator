7c87a017348d6abcc79db1436b7d785aab31634b
Gareth Murphy <garethcmurphy@gmail.com>
2018-10-06 20:12:37 +0200

    add test runner

--- index.ts
+++ index.ts
@@ -26,42 +26,79 @@ class MetadataCreator {
     return this.publish;
   }
 
-  getOrig() {
+  getOrig(inst: DefaultInstrument) {
     this.orig = new OrigDatablock();
     this.orig.createdAt = new Date();
+    this.orig.size = inst.sizeOfArchive;
+    this.orig.dataFileList = ["file1"];
+    this.orig.ownerGroup = "ingestor";
+    this.orig.accessGroups = ["ingestor"];
+    this.orig.createdBy = "ingestor";
+    this.orig.updatedBy = "ingestor";
+    this.orig.datasetId = "ingestor";
+    this.orig.rawDatasetId = "ingestor";
+    this.orig.derivedDatasetId = "ingestor";
+    this.orig.createdAt = new Date();
+    this.orig.updatedAt = new Date();
+    return this.orig;
   }
 
-  getLifeCycle() {
+  getLifeCycle(inst: DefaultInstrument) {
     this.lifecycle = new DatasetLifecycle();
     this.lifecycle.archivable = true;
+    return this.lifecycle;
   }
 
-  getDataset() {
+  getDataset(inst: DefaultInstrument) {
     this.dataset = new Dataset();
     this.dataset.contactEmail = "test@new.com";
+    this.dataset.pid =" string ";
+    this.dataset.owner = inst.creator;
+    this.dataset.ownerEmail =" string ";
+    this.dataset.orcidOfOwner =" string ";
+    this.dataset.contactEmail =" string ";
+    this.dataset.sourceFolder =" string ";
+    this.dataset.size = inst.sizeOfArchive;
+    this.dataset.packedSize = inst.sizeOfArchive;
+    this.dataset.creationTime = new Date();
+    this.dataset.type =" string ";
+    this.dataset.validationStatus =" string ";
+    this.dataset.keywords =["vanadium"];
+    this.dataset.description =" string ";
+    this.dataset.userTargetLocation =" string ";
+    this.dataset.classification =" string ";
+    this.dataset.license =" string ";
+    this.dataset.version =" string ";
+    this.dataset.doi =" string ";
+    this.dataset.isPublished =true;
+    this.dataset.ownerGroup =" string ";
+    this.dataset.accessGroups =["string"];
+    this.dataset.createdBy =" string ";
+    this.dataset.updatedBy =" string ";
+    this.dataset.createdAt = new Date();
+    this.dataset.updatedAt = new Date();
     return this.dataset;
   }
 
   mainloop() {
     const inst_array = ["sonde", "nmx"];
-    for ( const inst_tag of inst_array) {
+    for (const inst_tag of inst_array) {
       console.log(inst_tag);
       const inst_fact = new InstrumentFactory();
 
       const inst = inst_fact.createInstrument(inst_tag);
       console.log(inst.abbreviation);
-      const dat = this.getDataset();
-      this.getLifeCycle();
+      const dat = this.getDataset(inst);
+      const life =this.getLifeCycle(inst);
       const pub = this.getPublish(inst);
-      this.getOrig();
+      const orig= this.getOrig(inst);
       const key1 = "key" + inst;
-
 
       this.metadata[key1] = {
         dat: dat,
         pub: pub,
-        orig: pub,
-        life: pub
+        orig: orig,
+        life: life
       };
     }
     this.print();
