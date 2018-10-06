3b7091c0601951147331d9c37a7cc9395397d498
Gareth Murphy <garethcmurphy@gmail.com>
2018-10-06 05:12:07 +0200

    update types

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { PublishedData } from "./PublishedData";
 import { Dataset } from "./Dataset";
 import { OrigDatablock } from "./OrigDatablock";
 import { DatasetLifecycle } from "./DatasetLifecycle";
-var fs = require("fs");
+const fs = require("fs");
 
 class MetadataCreator {
   metadata: Object;
@@ -12,41 +12,50 @@ class MetadataCreator {
   lifecycle: DatasetLifecycle;
 
   constructor() {
-      this.metadata = this.publish;
+    this.metadata = {};
   }
 
-  getPublish(){
+  getPublish() {
     this.publish = new PublishedData();
     this.publish.creator = "J Smith";
     this.publish.publisher = "ESS";
     this.publish.affiliation = "ESS";
     this.publish.publicationYear = 2018;
     this.publish.pidArray = ["J Smith"];
+    return this.publish;
   }
 
-  getOrig(){
+  getOrig() {
     this.orig = new OrigDatablock();
     this.orig.createdAt = new Date();
   }
 
-  getLifeCycle(){
+  getLifeCycle() {
     this.lifecycle = new DatasetLifecycle();
     this.lifecycle.archivable = true;
   }
 
-
-  getDataset(){
+  getDataset() {
     this.dataset = new Dataset();
     this.dataset.contactEmail = "test@new.com";
+    return this.dataset;
   }
 
   mainloop() {
     for (let i = 0; i < 5; i++) {
       console.log(i);
-      this.getDataset();
+      const dat = this.getDataset();
       this.getLifeCycle();
-      this.getPublish();
+      const pub = this.getPublish();
       this.getOrig();
+      const key1 = "key" + i.toString();
+
+      this.metadata[key1] = {
+        dat: dat,
+        pub: pub,
+        orig: pub,
+        life: pub
+      };
     }
     this.print();
   }
@@ -56,8 +65,8 @@ class MetadataCreator {
   }
 
   print() {
-    console.log(this.publish);
-    const json = JSON.stringify(this.publish);
+    console.log(this.metadata);
+    const json = JSON.stringify(this.metadata);
     fs.writeFile("publish.json", json, function(err) {
       if (err) throw err;
       console.log("Saved!");
