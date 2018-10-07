f8e33653b7bac635efe2097c53b0c315f285e3f4
Gareth Murphy <garethcmurphy@gmail.com>
2018-10-07 22:13:18 +0200

    🙈🔖

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -5,6 +5,6 @@ import "mocha";
 describe("Hello function", () => {
   it("should return hello world", () => {
     const result = new Sonde();
-    expect(result).to.have.own.property("email");
+    expect(result).to.have.own.property("contactEmail");
   });
 });
