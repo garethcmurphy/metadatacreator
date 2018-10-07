71efd172f87e6fd516b7569e28ff4b990a094458
Gareth Murphy <garethcmurphy@gmail.com>
2018-10-07 21:23:17 +0200

    🙈✨

--- instrument.ts
+++ instrument.ts
@@ -1,5 +1,4 @@
 class DefaultInstrument {
-  email: string;
   owner: string;
   ownerEmail: string;
   orcidOfOwner: string;
@@ -29,9 +28,9 @@ class DefaultInstrument {
   dataDescription: string;
   abstract: string;
   resourceType: string;
-  thumbnail ="string";
-  sizeOfArchive= 22;
-  numberOfFiles= 21;
+  thumbnail = "string";
+  sizeOfArchive = 22;
+  numberOfFiles = 21;
   pidArray: string[];
   authors: string[];
   doiRegisteredSuccessfullyTime: string;
@@ -69,7 +68,6 @@ class Multigrid extends DefaultInstrument {
     this.userTargetLocation = "multigrid";
     this.sourceFolder = "multigrid/data/raw/MG_CNCS/07_14";
     this.creationLocation = this.userTargetLocation;
-    this.email = "test@kjd.com";
     this.abbreviation = "Multigrid";
     this.classification = " string ";
     this.license = "ESS";
@@ -89,12 +87,12 @@ class Multigrid extends DefaultInstrument {
     this.title = "Sample Data from " + this.abbreviation;
     this.url = this.url_fragment + this.abbreviation;
     this.dataDescription =
-      "https://github.com/ess-dmsc/ess_file_formats/wiki/SONDE";
+      "https://github.com/ess-dmsc/ess_file_formats/wiki/Multigrid-Data-Format-I";
     this.abstract =
       "This data was collected as part of BrightnESS, funded by the European Union \
                         Framework Programme for Research and Innovation Horizon 2020, under grant \
                         agreement 676548. It consists of test data for the detector.";
-    this.resourceType = "Comma Separated Variable (csv) files";
+    this.resourceType = "raw binary files in Multigrid format";
     this.pidArray = ["string"];
     this.authors = [this.owner];
     this.doiRegisteredSuccessfullyTime = "2018";
@@ -149,7 +147,6 @@ class Multiblade extends DefaultInstrument {
     this.sourceFolder =
       "multiblade/data/brightness/2017_06_BNC_MB16T_ElectronicsTests/DATA";
     this.creationLocation = this.userTargetLocation;
-    this.email = "test@kjd.com";
     this.abbreviation = "Multiblade";
     this.doi = this.doi_prefix + this.abbreviation;
     this.affiliation = "ESS";
@@ -190,101 +187,139 @@ class Multiblade extends DefaultInstrument {
     const folder_prefix = "multiblade/data/brightness/";
     this.source_folder_array = {
       "0001":
-        folder_prefix + "/2016_12_STF_MB15_GammaSensitivity/20161210_gammaSensData",
+        folder_prefix +
+        "/2016_12_STF_MB15_GammaSensitivity/20161210_gammaSensData",
       "0002":
-        folder_prefix + "/2017_04_STF_MB15_FastNeutronSensitivity/data_FastNeutron",
-      "0003":
-        folder_prefix + "/2017_06_BNC_MB16T_ElectronicsTests/DATA",
+        folder_prefix +
+        "/2017_04_STF_MB15_FastNeutronSensitivity/data_FastNeutron",
+      "0003": folder_prefix + "/2017_06_BNC_MB16T_ElectronicsTests/DATA",
       "0004":
         folder_prefix + "/2017_06_BNC_MB16T_ElectronicsTests/DATA_011_images",
-      "0005":
-        folder_prefix + "/2017_06_BNC_MB16T_ElectronicsTests/prove0620",
-      "0006":
-        folder_prefix + "/2017_06_BNC_MB16T_ElectronicsTests/prove0621",
+      "0005": folder_prefix + "/2017_06_BNC_MB16T_ElectronicsTests/prove0620",
+      "0006": folder_prefix + "/2017_06_BNC_MB16T_ElectronicsTests/prove0621",
       "0007":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/02_1_provaFirmware",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/02_1_provaFirmware",
       "0008":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_10_F_V_test",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_10_F_V_test",
       "0009":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_11_G_plexi_test",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_11_G_plexi_test",
       "0010":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_1_provaFirmware",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_1_provaFirmware",
       "0011":
         folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_2_A_Ni",
       "0012":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_3_B_DirectBeamTests",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_3_B_DirectBeamTests",
       "0013":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_4_B_DirectBeam",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_4_B_DirectBeam",
       "0014":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_5_C_DirectBeam_5mmAl",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_5_C_DirectBeam_5mmAl",
       "0015":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_6_D_Ir_align",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_6_D_Ir_align",
       "0016":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_7_E_Ir_collimated",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_7_E_Ir_collimated",
       "0017":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_8_E_Ir_slit",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_8_E_Ir_slit",
       "0018":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_9_E_Ir_test",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03_9_E_Ir_test",
       "0019":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03N_12_G_plexi",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/03N_12_G_plexi",
       "0020":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_10_I2_Si_divergent",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_10_I2_Si_divergent",
       "0021":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_11_L_FeSi_test",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_11_L_FeSi_test",
       "0022":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_1_E_Ir_test",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_1_E_Ir_test",
       "0023":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_2_E1_Ir_collimated",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_2_E1_Ir_collimated",
       "0024":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_3_H_Si_test",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_3_H_Si_test",
       "0025":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_4_H_Si_collimated",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_4_H_Si_collimated",
       "0026":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_5_H1_Si_collimated_HVtests",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_5_H1_Si_collimated_HVtests",
       "0027":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_6_I_FeSi_test",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_6_I_FeSi_test",
       "0028":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_7_I_FeSi_divergent",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_7_I_FeSi_divergent",
       "0029":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_8_I1_FeSi_divergent_magnet",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_8_I1_FeSi_divergent_magnet",
       "0030":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_9_I2_Si_divergent_test",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04_9_I2_Si_divergent_test",
       "0031":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04N_12_L_FeSi_collimated_OffSpec",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/04N_12_L_FeSi_collimated_OffSpec",
       "0032":
         folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05_10_P_mask",
       "0033":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05_11_Q_Si_divergent_highQ_test",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05_11_Q_Si_divergent_highQ_test",
       "0034":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05_1_M_FeSi_collimated_magnet_test",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05_1_M_FeSi_collimated_magnet_test",
       "0035":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05_2_M_FeSi_collimated_magnet",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05_2_M_FeSi_collimated_magnet",
       "0036":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05_3_N_ResW_test",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05_3_N_ResW_test",
       "0037":
         folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05_4_N_ResW",
       "0038":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05_5_N1_ResW_longRun",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05_5_N1_ResW_longRun",
       "0039":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05_6_O_PosStrip_test",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05_6_O_PosStrip_test",
       "0040":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05_7_O_PosStrip_linkN1",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05_7_O_PosStrip_linkN1",
       "0041":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05_8_O1_ResStrip",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05_8_O1_ResStrip",
       "0042":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05_9_P_mask_test",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05_9_P_mask_test",
       "0043":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05N_12_Q_Si_divergent_highQ",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05N_12_Q_Si_divergent_highQ",
       "0044":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05N_13_Q_Si_divergent_highQ_BeamOff",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/05N_13_Q_Si_divergent_highQ_BeamOff",
       "0045":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/06_1_R_Efficiency_50Hz_test",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/06_1_R_Efficiency_50Hz_test",
       "0046":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/06_2_R_Efficiency_50Hz",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/06_2_R_Efficiency_50Hz",
       "0047":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/06_3_R12_Efficiency_25Hz_test",
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/06_3_R12_Efficiency_25Hz_test",
       "0048":
-        folder_prefix + "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/06_4_R2_Efficiency_25Hz"
+        folder_prefix +
+        "/2017_10_ISIS_MB16S_ReflectometryAtCRISP/06_4_R2_Efficiency_25Hz"
     };
   }
 }
@@ -298,10 +333,8 @@ class Sonde extends DefaultInstrument {
     this.contactEmail = this.ownerEmail;
     this.principalInvestigator = this.owner;
     this.userTargetLocation = "multiblade";
-    this.sourceFolder =
-      "sonde/IFE_june_2018/data/S13";
+    this.sourceFolder = "sonde/IFE_june_2018/data/S13";
     this.creationLocation = this.userTargetLocation;
-    this.email = "test@kjd.com";
     this.abbreviation = "SONDE";
     this.doi = this.doi_prefix + this.abbreviation;
     this.affiliation = "ESS";
@@ -360,17 +393,26 @@ class Sonde extends DefaultInstrument {
       "0018": "sonde/IFE_june_2018/data/temp/t50",
       "0019": "sonde/IFE_oct_2017/from_EFU_PC/EFU_data",
       "0020": "sonde/IFE_oct_2017/from_lenovo_laptop/gamma/test_saved_data",
-      "0021": "sonde/IFE_oct_2017/from_lenovo_laptop/IDEASTestbench_BETA_V0_24-x86-ALL/conf/CZTCam",
-      "0022": "sonde/IFE_oct_2017/from_lenovo_laptop/IDEASTestbench_BETA_V0_24-x86-ALL/conf/Default",
-      "0023": "sonde/IFE_oct_2017/from_lenovo_laptop/IDEASTestbench_BETA_V0_24-x86-ALL/conf/Galao-IDE3380_SIPHRA",
-      "0024": "sonde/IFE_oct_2017/from_lenovo_laptop/IDEASTestbench_BETA_V0_24-x86-ALL/conf/Galao-IDE3380_SIPHRA/reg_conf",
-      "0025": "sonde/IFE_oct_2017/from_lenovo_laptop/IDEASTestbench_BETA_V0_24-x86-ALL/conf/Galao-VATA64HDR16",
-      "0026": "sonde/IFE_oct_2017/from_lenovo_laptop/IDEASTestbench_BETA_V0_24-x86-ALL/conf/GammaProcessor",
-      "0027": "sonde/IFE_oct_2017/from_lenovo_laptop/IDEASTestbench_BETA_V0_24-x86-ALL/conf/INX-500",
-      "0028": "sonde/IFE_oct_2017/from_lenovo_laptop/IDEASTestbench_BETA_V0_24-x86-ALL/conf/ROSMAP-MP",
-      "0029": "sonde/IFE_oct_2017/from_lenovo_laptop/IDEASTestbench_BETA_V0_24-x86-ALL/conf/XCS-1000",
-      "0030": "sonde/IFE_oct_2017/from_lenovo_laptop/IDEASTestbench_BETA_V0_24-x86-ALL/logs"
-
+      "0021":
+        "sonde/IFE_oct_2017/from_lenovo_laptop/IDEASTestbench_BETA_V0_24-x86-ALL/conf/CZTCam",
+      "0022":
+        "sonde/IFE_oct_2017/from_lenovo_laptop/IDEASTestbench_BETA_V0_24-x86-ALL/conf/Default",
+      "0023":
+        "sonde/IFE_oct_2017/from_lenovo_laptop/IDEASTestbench_BETA_V0_24-x86-ALL/conf/Galao-IDE3380_SIPHRA",
+      "0024":
+        "sonde/IFE_oct_2017/from_lenovo_laptop/IDEASTestbench_BETA_V0_24-x86-ALL/conf/Galao-IDE3380_SIPHRA/reg_conf",
+      "0025":
+        "sonde/IFE_oct_2017/from_lenovo_laptop/IDEASTestbench_BETA_V0_24-x86-ALL/conf/Galao-VATA64HDR16",
+      "0026":
+        "sonde/IFE_oct_2017/from_lenovo_laptop/IDEASTestbench_BETA_V0_24-x86-ALL/conf/GammaProcessor",
+      "0027":
+        "sonde/IFE_oct_2017/from_lenovo_laptop/IDEASTestbench_BETA_V0_24-x86-ALL/conf/INX-500",
+      "0028":
+        "sonde/IFE_oct_2017/from_lenovo_laptop/IDEASTestbench_BETA_V0_24-x86-ALL/conf/ROSMAP-MP",
+      "0029":
+        "sonde/IFE_oct_2017/from_lenovo_laptop/IDEASTestbench_BETA_V0_24-x86-ALL/conf/XCS-1000",
+      "0030":
+        "sonde/IFE_oct_2017/from_lenovo_laptop/IDEASTestbench_BETA_V0_24-x86-ALL/logs"
     };
   }
 }
@@ -386,7 +428,6 @@ class Nmx extends DefaultInstrument {
     this.userTargetLocation = "NMX";
     this.sourceFolder = "nmx/data/h5/analyzed/dead200dead600/IFE_2015_Feb";
     this.creationLocation = this.userTargetLocation;
-    this.email = "test@kjd.com";
     this.abbreviation = "NMX";
     this.doi = this.doi_prefix + this.abbreviation;
     this.affiliation = "ESS";
@@ -431,7 +472,8 @@ class Nmx extends DefaultInstrument {
       "0004": "nmx/data/h5/analyzed/thresh100_150/IFE_2015_Feb",
       "0005": "nmx/data/h5/analyzed/thresh100_150/IFE_2016_Jun",
       "0006": "nmx/data/h5/analyzed/thresh100_150/IFE_2016_Nov",
-      "0007": "nmx/data/h5/analyzed/thresh100_150/IFE_2016_Nov/APZ_threshold_scan",
+      "0007":
+        "nmx/data/h5/analyzed/thresh100_150/IFE_2016_Nov/APZ_threshold_scan",
       "0010": "nmx/data/h5/analyzed/thresh100_150/IFE_2016_Nov/Calibration",
       "0011": "nmx/data/h5/analyzed/thresh100_150/IFE_2016_Nov/Efficiency",
       "0012": "nmx/data/h5/analyzed/thresh100_150/IFE_2016_Nov/Pattern",
