ab4805d01a504519a73e6eca57a56e18fcdfa0b2
Gareth Murphy <garethcmurphy@gmail.com>
2018-10-25 20:10:39 +0200

    Add check for metadata tag

--- src/WebScrape.ts
+++ src/WebScrape.ts
@@ -4,7 +4,5 @@ const url = "https://stf02.nuclear.lu.se/SoNDe+Testbeams/";
 scraper
   .get(url)
   .then(function(tableData) {
-
-
     fs.writeFileSync("./results.json", JSON.stringify(tableData, null, 2), "utf-8");
   });
