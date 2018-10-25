e3d7d94624cfc1287518e436f91d797823501ac4
Gareth Murphy <garethcmurphy@gmail.com>
2018-10-25 20:10:10 +0200

    Add check for metadata tag

--- src/WebScrape.ts
+++ src/WebScrape.ts
@@ -1,21 +1,10 @@
 const fs = require("fs");
-var scraper = require("table-scraper");
+const scraper = require("table-scraper");
 const url = "https://stf02.nuclear.lu.se/SoNDe+Testbeams/";
 scraper
   .get(url)
   .then(function(tableData) {
 
 
-    fs.writeFileSync("./results.json", JSON.stringify(tableData, null, 2), (err) => {
-      console.log("Dumped news bits to file");
-    });
-    /*
-       tableData ===
-        [
-          [
-            { State: 'Minnesota', 'Capital City': 'Saint Paul', 'Pop.': '3' },
-            { State: 'New York', 'Capital City': 'Albany', 'Pop.': 'Eight Million' }
-          ]
-        ]
-    */
+    fs.writeFileSync("./results.json", JSON.stringify(tableData, null, 2), "utf-8");
   });
