9bcdb9541805a40dc40a48bc93429fb31fea2dfd
Gareth Murphy <garethcmurphy@gmail.com>
2018-11-25 09:12:20 +0100

    add thumbnail :bug:

--- src/ThumbnailImage.ts
+++ src/ThumbnailImage.ts
@@ -1,10 +1,21 @@
 
 
+const fs = require("fs");
+
 class ThumbnailImage {
 
   image: string;
 
   constructor(){
-    this.image = "";
+    const filename = "./assets/image.png"
+    this.image = this.base64_encode(filename);
+    
   }
+
+  base64_encode(file: string) {
+    // read binary data
+    const bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+}
 }
