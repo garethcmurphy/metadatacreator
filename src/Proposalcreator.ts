86e93997bf39f8549e232ac385f8dbe9d2532012
Gareth Murphy <garethcmurphy@gmail.com>
2018-11-27 13:24:04 +0100

    update proposal creator

--- src/Proposalcreator.ts
+++ src/Proposalcreator.ts
@@ -5,21 +5,32 @@ const fs = require("fs");
 
 export class ProposalCreator {
   metadata: Object;
-  policy: Proposal;
+  proposal: Proposal;
   manager_list = ["gareth.murphy@esss.se"];
 
   constructor() {
     this.metadata = {};
   }
 
-  getPolicy(inst: DefaultInstrument, tag: string) {
-    this.policy = new Proposal();
-    this.policy.ownerGroup = inst.ownerGroup;
-    this.policy.accessGroups = inst.accessGroups;
-    this.policy.createdBy = inst.createdBy;
-    this.policy.updatedBy = inst.updatedBy;
-    this.policy.updatedAt = inst.updatedAt;
-    return this.policy;
+  getProposal(inst: DefaultInstrument, tag: string) {
+    this.proposal = new Proposal();
+    this.proposal.proposalId = inst.ownerGroup;
+    this.proposal.pi_email = inst.ownerGroup;
+    this.proposal.pi_firstname = inst.ownerGroup;
+    this.proposal.pi_lastname = inst.ownerGroup;
+    this.proposal.email = inst.ownerGroup;
+    this.proposal.firstname = inst.ownerGroup;
+    this.proposal.lastname = inst.ownerGroup;
+    this.proposal.title = inst.ownerGroup;
+    this.proposal.abstract = inst.ownerGroup;
+    this.proposal.attachments = [inst.ownerGroup];
+    this.proposal.MeasurementPeriodList = [inst.ownerGroup];
+    this.proposal.ownerGroup = inst.ownerGroup;
+    this.proposal.accessGroups = inst.accessGroups;
+    this.proposal.createdBy = inst.createdBy;
+    this.proposal.updatedBy = inst.updatedBy;
+    this.proposal.updatedAt = inst.updatedAt;
+    return this.proposal;
   }
 
   mainloop() {
@@ -30,10 +41,10 @@ export class ProposalCreator {
       const inst = inst_fact.createInstrument(inst_tag);
       console.log(inst.abbreviation);
 
-      const policy = this.getPolicy(inst, "example");
+      const proposal = this.getProposal(inst, "example");
       const key1 = "key" + inst_tag;
       this.metadata[key1] = {
-        policy: policy
+        proposal: proposal
       };
     }
     this.print();
@@ -42,12 +53,12 @@ export class ProposalCreator {
   print() {
     //console.log(this.metadata);
     const json = JSON.stringify(this.metadata, null, 4);
-    fs.writeFile("policy.json", json, err => {
+    fs.writeFile("proposal.json", json, err => {
       if (err) throw err;
       console.log("The file has been saved!");
     });
   }
 }
 
-const sam = new PolicyCreator();
+const sam = new ProposalCreator();
 sam.mainloop();
