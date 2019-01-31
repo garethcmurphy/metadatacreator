3227a20ff6d13361158f556ede322f52eccca93a
Gareth Murphy <garethcmurphy@gmail.com>
2019-01-31 11:58:04 +0100

    add sample

--- src/GetH5Info.ts
+++ src/GetH5Info.ts
@@ -13,49 +13,50 @@ export class GetH5Info {
   }
   getInfo(fileName: string) {
     const file = new hdf5.File(fileName, Access.ACC_RDONLY);
-      let nexusInfo = {
-          title: "",
-          description: "",
-          owners: [""],
-      };
-    const  rootGroups = file.getMemberNames();
-      console.log("rootGroups",rootGroups);
-      if (rootGroups.includes("entry") ) {
-
-    let group = file.openGroup("/entry/");
-    let members = group.getMemberNames();
-    // console.log(members);
-    
-    if (members.includes("title")) {
-        nexusInfo.title= h5lt.readDataset(group.id, "title"); 
-    }
-    if (members.includes("ESS_users") ) {
-      group = file.openGroup("/entry/ESS_users/");
-      members = group.getMemberNames();
+    let nexusInfo = {
+      title: "",
+      description: "",
+      owners: [""]
+    };
+    const rootGroups = file.getMemberNames();
+    console.log("rootGroups", rootGroups);
+    if (rootGroups.includes("entry")) {
+      let group = file.openGroup("/entry/");
+      let members = group.getMemberNames();
       // console.log(members);
-      const array = h5lt.readDataset(group.id, "name");
-        nexusInfo.owners = array;
-      console.log(array);
-    } else {
-        console.log("no ESS_Users");
-    }
 
-          if (members.includes("sample")) {
-      const sampleGroup = file.openGroup("/entry/sample/");
-      const sampleMembers = sampleGroup.getMemberNames();
-
-              if (sampleMembers.includes("sample")) {
-                  nexusInfo.description = h5lt.readDataset(group.id, "description");
-              }
-          }
-          
-      } else { 
-          console.log("no field called entry in nxs groups");
+      if (members.includes("title")) {
+        nexusInfo.title = h5lt.readDataset(group.id, "title");
       }
-      file.close();
-      return nexusInfo;
+      if (members.includes("ESS_users")) {
+        const userGroup = file.openGroup("/entry/ESS_users/");
+        const userMembers = userGroup.getMemberNames();
+        // console.log(members);
+        const array = h5lt.readDataset(userGroup.id, "name");
+        nexusInfo.owners = array;
+        console.log(array);
+      } else {
+        console.log("no ESS_Users");
+      }
+
+      if (members.includes("sample")) {
+        const sampleGroup = file.openGroup("/entry/sample/");
+        const sampleMembers = sampleGroup.getMemberNames();
+        console.log (sampleMembers)
+
+        if (sampleMembers.includes("description")) {
+          nexusInfo.description = h5lt.readDataset(sampleGroup.id, "description");
+          console.log("description", nexusInfo.description);
+        }
+      } else {
+          console.log("no sample");
+      }
+    } else {
+      console.log("no field called entry in nxs groups");
+    }
+    file.close();
+    return nexusInfo;
   }
-    
 }
 
 if (require.main === module) {
