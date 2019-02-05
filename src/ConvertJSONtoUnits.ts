8f85341575a786dfe863ec4bd4646a4621a8ba5f
Gareth Murphy <garethcmurphy@gmail.com>
2019-02-05 08:01:04 +0100

    add units detector

--- src/ConvertJSONtoUnits.ts
+++ src/ConvertJSONtoUnits.ts
@@ -9,16 +9,40 @@ class ConvertJSONtoUnits {
     }
 
     convertToUnits(jsonObject:Object){
+        const unitsObject = {};
         for (var key in jsonObject) {
             if (jsonObject.hasOwnProperty(key)) {
-                console.log(key + " -> " + jsonObject[key]);
+                unitsObject[key] = {
+                    "v": jsonObject[key],
+                    "u": this.units_lookup(key)
+                }
             }
         }
+        console.log(unitsObject);
+        return unitsObject;
+    }
+
+    units_lookup(key: string) {
+        console.log("gm", key);
+        let units = "unknown";
+        switch (key){
+            case "pressure":
+                units = "Pascale";
+                break;
+            case "temperature":
+                units = "Kelvin";
+                break;
+            case "temp":
+                units = "Kelvin";
+                break;
+            default:
+                units = "unknown";
+        }
+        return units;
     }
 }
 
 if (require.main === module) {
     const conv = new ConvertJSONtoUnits()
     conv.convertToUnits(conv.init_json);
-
 }
