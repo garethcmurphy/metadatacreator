c4a912b3b1257c7d4281dbdb7c7d910d3b726b7b
Gareth Murphy <garethcmurphy@gmail.com>
2019-02-26 02:47:40 +0100

    add fix to unit tests

--- test/multigrid.spec.ts
+++ test/multigrid.spec.ts
@@ -8,9 +8,9 @@ describe("check multigrid", () => {
     const result = new Multigrid();
     const sourceFolder = "/users/detector/experiments/multigrid/data/raw/SNS_Aug18/efu_dump/9-2 15-00 US_White";
     const fileInfo = new FilesInfo("demo");
-    fileInfo.experimentDateTime = new Date("2018-07-05T13:00:00.000Z");
+    fileInfo.experimentDateTime = new Date("2018-07-05T03:00:00.000Z");
     fileInfo.sourceFolder = sourceFolder;
-    expect(result.getTime(fileInfo)).to.equal("2018-09-01T13:00:00.000Z");
+    expect(result.getTime(fileInfo)).to.equal("2018-09-02T15:00:00.000Z");
   });
 
   it("should parse old time", () => {
@@ -19,6 +19,6 @@ describe("check multigrid", () => {
     const fileInfo = new FilesInfo("demo");
     fileInfo.experimentDateTime = new Date("2018-07-05T13:00:00.000Z");
     fileInfo.sourceFolder = sourceFolder;
-    expect(result.getTime(fileInfo)).to.equal("2018-07-05T13:00:00.000Z");
+    expect(result.getTime(fileInfo)).to.equal("20180801");
   });
 });
