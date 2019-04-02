a09bab0b35d33bc202f13c48ac59a12d4f9b173e
Gareth Murphy <garethcmurphy@gmail.com>
2019-04-02 22:16:34 +0200

    add csv

---
+++ src/CSVtoJSON.ts
@@ -0,0 +1,16 @@
+class CSVtoJSON {
+    filename: string;
+
+
+
+}
+
+
+
+if (require.main === module) {
+    const a = new CSVtoJSON();
+    const inputDirectory = "./input/"
+    const fileName = "./input/scan_5800.csv"
+    const outputFileName = fileName.replace("xlsx","json");
+    //a.get_file(fileName);
+  }
