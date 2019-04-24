804fec26d7da7300e41e3f0a1f25c224f782db81
Gareth Murphy <garethcmurphy@gmail.com>
2019-04-24 10:10:12 +0200

    add loop to generate metadata

--- src/GetNewFiles.ts
+++ src/GetNewFiles.ts
@@ -1,13 +1,29 @@
 import { readdirSync } from "fs";
+import { Dataset } from "../shared/sdk";
 class GetNewFiles {
+  names: Array<string>;
+  basename = "demo/";
+  directoryName = "v20/2019_04_05/";
   readDirectory() {
-    const directoryName = "demo/v20/";
-    const names = readdirSync(directoryName);
-      console.log(names);
+    this.names = readdirSync(this.basename + this.directoryName);
+    console.log(this.names.slice(0, 3));
+  }
+
+  loopFiles() {
+    //const ds = new Dataset();
+    const indexStart = 45;
+    let fileObject = {};
+    for (let i = 0; i < this.names.length; i++) {
+      let newIndex = indexStart + i;
+      fileObject[newIndex.toString().padStart(4, "0")] =
+        this.directoryName + this.names[i];
+    }
+    console.log(JSON.stringify(fileObject, null, 2));
   }
 }
 
 if (require.main === module) {
-    const get = new GetNewFiles();
-    get.readDirectory();
+  const get = new GetNewFiles();
+  get.readDirectory();
+  get.loopFiles();
 }
