e468c9f543bc0531d00e99650c39cfe32774c691
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-18 20:27:23 +0200

    rename unit tests

--- tests/dsc.spec.ts
+++ tests/dsc.spec.ts
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import "mocha";
 import { DSC } from "../src/DSC";
 
-describe("check sonde", () => {
+describe("check dsc", () => {
   it("should have property contactemail", () => {
     const result = new DSC();
     expect(result).to.have.property("contactEmail");
--- tests/proposal.spec.ts
+++ tests/proposal.spec.ts
@@ -3,7 +3,7 @@ import "mocha";
 
 import { BeamInstrumentation } from "./../src/BeamInstrumentation";
 
-describe("check sonde", () => {
+describe("check proposal", () => {
   it("should have property contactemail", () => {
     const result = new BeamInstrumentation();
     expect(result).to.have.property("contactEmail");
