4b17fe61b17d94f6b55246e8e3066b08714f9e7b
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-18 11:50:48 +0200

    add unit tests

--- tests/metadatacreator.spec.ts
+++ tests/metadatacreator.spec.ts
@@ -2,9 +2,18 @@ import { expect } from "chai";
 import "mocha";
 import { MetadataCreator } from "../src/metadatacreator";
 
+import { FilesInfo } from "./../src/filesinfo";
+import { Sonde } from "./../src/Sonde";
+
 describe("check metadatacreator", () => {
   it("should have property metadata", () => {
     const result = new MetadataCreator();
     expect(result).to.have.property("metadata");
+    const instrument = new Sonde();
+    const tag = "string";
+    const sourceFolder = "demo/";
+
+    const filesInfo = new FilesInfo(sourceFolder);
+    result.getDataset(instrument, tag, filesInfo);
   });
 });
